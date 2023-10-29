1d80709f2f7f14b689e0d3aa29c405d40c50e31f
Chirath <chirath.nishshanka@gmail.com>
2023-10-29 07:21:00 +0530

    Algae L-System implement

---
+++ systems/algae.py
@@ -0,0 +1,47 @@
+'''
+ variables A, B 
+ constants None 
+ axiom     A 
+ rules (A -> AB), (B -> A)
+'''
+
+def gen_algae(axiom,rules={"A":"AB","B":"A"}, verborse=True, iterations=8):
+    next_generation = "" 
+    current_generation = axiom 
+    generations = [] 
+
+    for n in range(0, iterations):
+        for i in current_generation:
+            if i in rules:
+                next_generation += rules[i] 
+            else:
+                next_generation += i 
+        
+        if verborse:
+            print("n = ", n, current_generation)
+
+        generations.append(current_generation)
+        current_generation = next_generation
+        next_generation = ""
+    
+    return generations
+
+def assert_gen_algae():
+    expected = [
+        "A",
+        "AB",
+        "ABA",
+        "ABAAB",
+        "ABAABABA",
+        "ABAABABAABAAB",
+        "ABAABABAABAABABAABABA",
+        "ABAABABAABAABABAABABAABAABABAABAAB",
+    ] 
+    actual = gen_algae("A", verborse=False)
+    for i, row in enumerate(actual):
+        for j, l in enumerate(row):
+            expected_row = expected[i] 
+            assert l == expected_row[j] 
+
+assert_gen_algae()
+
