597adc1990315e63b14c895938eabd7b7c2d85b8
Chirath <chirath.nishshanka@gmail.com>
2023-10-29 08:04:41 +0530

    Implement fractal plant

---
+++ systems/plant.py
@@ -0,0 +1,9 @@
+from lsystem import gen_lsystem 
+
+AXIOM = "X" 
+RULES = {"X":"F+[[X]-X]-F[-FX]+X", "F":"FF"}
+
+def gen_fractal_plant(axiom=AXIOM, rules=RULES, verbose=True, iterations=6):
+    return gen_lsystem(axiom, rules, iterations, verbose) 
+
+
