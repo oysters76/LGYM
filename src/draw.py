7bbdfd3561a2f773392229240bb928fd0186cc75
Chirath <chirath.nishshanka@gmail.com>
2023-10-29 09:50:28 +0530

    Implement draw for fractal plant l-system

--- src/draw.py
+++ src/draw.py
@@ -47,3 +47,56 @@ def draw_si_traingle(generation):
             right(60)
     done()
 
+def draw_tree(generation):
+    clearscreen() 
+    speed("fastest")
+    mode("logo")
+    up()
+    setpos(0,-400)
+    down()
+    stack = [] 
+    for token in generation:
+        if token == "0":
+            forward(1) 
+        if token == "1":
+            forward(2) 
+        if token == "[":
+            stack.append([position(), heading()])
+            left(45) 
+        if token == "]":
+            p, h = stack.pop()
+            up()
+            goto(p) 
+            setheading(h)
+            right(45)
+            down()
+    done()
+
+def draw_fractal_plant(generation):
+    clearscreen() 
+    speed("fastest")
+    mode("logo")
+    up()
+    setpos(-400,-400)
+    down()
+    stack = []
+    for token in generation:
+        if token == "X":
+            continue 
+        if token == "F":
+            forward(5) 
+        if token == "+":
+            left(25) 
+        if token == "-":
+            right(25)
+        if token == "[":
+            stack.append([position(), heading()])
+        if token == "]":
+            p, h = stack.pop()
+            up()
+            goto(p)
+            setheading(h) 
+            down()
+    done()
+
+
