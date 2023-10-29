f5cecabde41cf99884eeb98e9b9ab15195edba63
Chirath <chirath.nishshanka@gmail.com>
2023-10-29 07:57:51 +0530

    Implement sierpinski arrowhead

--- systems/sierpinski.py
+++ systems/sierpinski.py
@@ -8,4 +8,10 @@ RULES = {"F":"F−G+F+G−F","G":"GG"}
 def gen_sierpinski(axiom=AXIOM, rules=RULES,verbose=True, iterations=6):
     return gen_lsystem(axiom, rules, iterations, verbose)
 
-    
+
+A_AXIOM = "A"
+A_RULES = {"A":"B−A−B", "B":"A+B+A"}
+
+def gen_sierpinski_arrowhead(axiom=A_AXIOM, rules=A_RULES, verbose=True, iterations=6):
+    return gen_lsystem(axiom, rules, iterations, verbose)
+
